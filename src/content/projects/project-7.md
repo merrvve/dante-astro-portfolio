a49179418f7f5bc2a4bcfbf7455d0411dc6c5346
mervs <mervsen@gmail.com>
2024-05-04 14:00:23 +0300

    ors project add

---
+++ src/content/projects/project-7.md
@@ -0,0 +1,20 @@
+---
+title: 'Vehicle Routing Problem - Open Route Service API Integration'
+description: I have been involved in the carriers' route optimization microservice of a project that integrates multiple sellers and carriers.
+publishDate: 'Feb 02 2023'
+seo:
+  image:
+    src: '/project-1.jpg'
+    alt: Project preview
+---
+
+<!-- ![Project preview](/marsev.png) -->
+
+**Project Overview:**
+I was involved in the carriers route optimization microservice of a project that integrated multiple vendors and carriers and was successfully integrated to create [Open Route Service API](https://openrouteservice.org/dev/#/api-docs/optimization/post). routes of multiple carriers with time window and capacity constraints.
+
+I've been interested in constraint issues ever since, and have [a side project](/projects/project-4) to create duty lists for doctors with unavailable day constraint.
+
+## Technology Stack
+
+- Django Ninja
--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -94,7 +94,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Welcome',
-        text: "Since childhood, I've been captivated by coding, and for the past two years, I've been freelancing as a full stack web developer. I've worked on diverse projects, improving my skills in various programming languages and frameworks. Right now, I'm working with <strong>Angular and Django.</strong> Aside from web development, I also have experience in data analysis and web scraping.<br /> Check out <a href='/projects'> my projects </a> to see what I've been up to,<br /> <a href='/about'>Learn more about me, </a><br />Download <a href='devcv-2024.pdf'> my resume</a> or <br /> Visit <a href='https://github.com/merrvve'>my Github.</a>  <br />Feel free to contact me by email: <a href='mailto:mervsen@gmail.com'>mervsen@gmail.com</a></a>",
+        text: "Since childhood, I've been intersted in coding, and for the past two years, I've been freelancing as a full stack web developer. I've worked on diverse projects, improving my skills in various programming languages and frameworks. Right now, I'm working with <strong>Angular and Django.</strong> Aside from web development, I also have experience in data analysis and web scraping.<br /> Check out <a href='/projects'> my projects </a> to see what I've been up to,<br /> <a href='/about'>Learn more about me, </a><br />Download <a href='devcv-2024.pdf'> my resume</a> or <br /> Visit <a href='https://github.com/merrvve'>my Github.</a>  <br />Feel free to contact me by email: <a href='mailto:mervsen@gmail.com'>mervsen@gmail.com</a></a>",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
